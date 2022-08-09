d9262bc57a5dbf547f7f8a1e5f2fb924ff6cfd84
nguyen.aislab <nguyen.aislab@gmail.com>
2022-08-09 12:38:59 +0900

    fix padding bug

--- src/components/LangBtn.js
+++ src/components/LangBtn.js
@@ -1,37 +1,38 @@
-import React from 'react'
-import { DropDownIcon } from './SvgIcons';
+import React from "react";
+import { DropDownIcon } from "./SvgIcons";
 
 export default function LangBtn(props) {
-    let cur = props.cur_lang ? props.cur_lang : 0;
-    let langs = props.langs
-    return (
-        <div className="py-2 flex space-x-2 justify-center content-center items-center">
-            <div className="dropdown relative align-middle h-fit">
-              <button
-                className="dropdown-toggle inline-block h-auto px-4 py-1.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:text-white focus:bg-gray-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out flex items-center whitespace-nowrap"
-                type="button"
-                id="dropdownMenuSmallButton"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+  let cur = props.cur_lang ? props.cur_lang : 0;
+  let langs = props.langs;
+  return (
+    <div className="py-2 flex space-x-2 justify-center content-center items-center">
+      <div className="dropdown relative align-middle h-fit">
+        <button
+          className="dropdown-toggle inline-block h-auto px-4 py-1.5 bg-white text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:text-white focus:bg-gray-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out flex items-center whitespace-nowrap"
+          type="button"
+          id="dropdownMenuSmallButton"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {langs[cur]}
+          {DropDownIcon}
+        </button>
+        <ul
+          className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none"
+          aria-labelledby="dropdownMenuSmallButton"
+        >
+          {langs.map((l) => (
+            <li>
+              <a
+                className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                href="#"
               >
-                {langs[cur]}
-                {DropDownIcon}
-              </button>
-              <ul className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none"
-                aria-labelledby="dropdownMenuSmallButton"
-              >
-                  {langs.map(l => (
-                      <li>
-                      <a
-                        className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-                        href="#"
-                      >
-                        {l}
-                      </a>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div>
-    )
+                {l}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/components/NavBarCore.js
+++ src/components/NavBarCore.js
@@ -1,15 +1,21 @@
 import React from "react";
 import LangBtn from "./LangBtn";
 import NavButton from "./NavButton";
-import { AboutIcon, BlogIcon, HomeIcon, InternalIcon, ResearchIcon } from "./SvgIcons";
+import {
+  AboutIcon,
+  BlogIcon,
+  HomeIcon,
+  InternalIcon,
+  ResearchIcon,
+} from "./SvgIcons";
 
 export default function NavBarCore(props) {
   let mobileMenu = React.createRef();
-  let menuColor = props.color? props.color : 'transparent';
+  let menuColor = props.color ? props.color : "transparent";
 
   return (
     <div className="container flex flex-wrap justify-between items-center mx-auto">
-      <a href="/" className="flex items-center ml-3">
+      <a href="/" className="flex items-center">
         <img
           src="https://img.upanh.tv/2022/05/05/unnamed__1_-removebg-preview.png"
           className="h-6"
@@ -19,62 +25,71 @@ export default function NavBarCore(props) {
           AIS Lab.
         </span>
       </a>
-      <button
-        data-collapse-toggle="mobile-menu"
-        onClick={() => {
-          mobileMenu.current.classList.toggle("hidden");
-        }}
-        type="button"
-        className="inline-flex items-center p-2 mx-3 text-sm text-white rounded-lg md:hidden hover:text-gray-500 hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="mobile-menu"
-        aria-expanded="false"
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
+      <div className="inline-flex items-center md:hidden">
+        <button
+          data-collapse-toggle="mobile-menu"
+          onClick={() => {
+            mobileMenu.current.classList.toggle("hidden");
+          }}
+          type="button"
+          className="p-2 mx-3 text-sm text-white rounded-lg hover:text-gray-500 hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
         >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-        <svg
-          className="hidden w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </button>
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <svg
+            className="hidden w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <LangBtn langs={["EN", "JP", "VN"]} cur_lang={0} />
+      </div>
       <div
         className={`hidden bg-${menuColor} w-full md:block md:w-auto`}
         id="mobile-menu"
         ref={mobileMenu}
       >
-        <ul className="flex flex-col md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-          <NavButton name={"Home"} icon={HomeIcon} href={'/'}/>
-          <NavButton name={"News"} icon={BlogIcon} href={'/news'}/>
-          <NavButton name={"Research"} icon={ResearchIcon} href={'/research'}/>
-          <NavButton name={"About"} icon={AboutIcon} href={'/about'}/>
-          <NavButton name={"Internal"} icon={InternalIcon} href={'/internal/admin'}/>
-          <div className="flex space-x-2 justify-center content-center py-2">
+        <ul className="flex flex-col md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium">
+          <NavButton name={"Home"} icon={HomeIcon} href={"/"} />
+          <NavButton name={"News"} icon={BlogIcon} href={"/news"} />
+          <NavButton name={"Research"} icon={ResearchIcon} href={"/research"} />
+          <NavButton name={"About"} icon={AboutIcon} href={"/about"} />
+          <NavButton
+            name={"Internal"}
+            icon={InternalIcon}
+            href={"/internal/admin"}
+          />
+          <div className="flex justify-center items-center content-center">
             <button
               type="button"
-              className="inline-block px-5 py-0.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-white hover:text-gray-500 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+              className="block ml-5 px-5 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-white hover:text-gray-500 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
             >
               Apply
             </button>
           </div>
-          <LangBtn langs={['EN', 'JP', 'VN']} cur_lang={0}/>
+          <span className="hidden md:flex justify-center">
+            <LangBtn langs={["EN", "JP", "VN"]} cur_lang={0} />
+          </span>
         </ul>
       </div>
     </div>
