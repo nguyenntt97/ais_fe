1bbc60f9ff338456b39109faafbe8865e732dfbf
nguyen.aislab <nguyen.aislab@gmail.com>
2022-08-10 11:36:27 +0900

    fix reload behavior for page redirect

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function NavButton(props) {
-    let name = props.name
-    let icon = props.icon
-    let href = props.href
-    return (
-        <div className="flex space-x-2 justify-center content-center">
-            <button
-              type="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              onClick={() => (window.location.href = href)}
-              className="block px-0.5 py-1.5 bg-transparent text-white font-sans font-semibold text-xs leading-tight md:hover:text-gray-300"
-            >
-              {icon}
-              &nbsp;{name}
-            </button>
-          </div>
-    )
+  let navigate = useNavigate();
+
+  let name = props.name;
+  let icon = props.icon;
+  let href = props.href;
+  return (
+    <div className="flex space-x-2 justify-center content-center">
+      <button
+        type="button"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="light"
+        onClick={() => navigate(href)}
+        className="block px-0.5 py-1.5 bg-transparent text-white font-sans font-semibold text-xs leading-tight md:hover:text-gray-300"
+      >
+        {icon}
+        &nbsp;{name}
+      </button>
+    </div>
+  );
 }
